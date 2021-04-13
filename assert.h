4d1f275758c1e5ae224e9645934edc4ae8617783
straple <54230867+Straple@users.noreply.github.com>
2021-04-13 15:40:52 +0300

    init Timer

---
+++ assert.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#define DEBUG_MODE
+
+#ifdef DEBUG_MODE
+
+#define ASSERT(condition, message) _STL_VERIFY(condition, message)
+
+#else
+
+#define ASSERT(condition, message) condition
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,31 @@
+﻿
+#include "time.h"
+
+#include <iostream>
+using namespace std;
+
+#pragma optimize("", off)
+
+int main() {
+    
+    Timer t;
+    for (int i = 0; i < 1e8; i++) {
+        
+    }
+
+    cout << t << "\n"; // 0.171692s
+
+    t.reset();
+
+    for (int i = 0; i < 1e8; i++) {
+        hypot(12, 2);
+    }
+    cout << t << "\n"; // 6.9618s
+
+    t.reset();
+
+    for (int i = 0; i < 1e8; i++) {
+        sqrt(12 * 12 + 2 * 2);
+    }
+    cout << t << "\n"; // 0.801943s
+}
